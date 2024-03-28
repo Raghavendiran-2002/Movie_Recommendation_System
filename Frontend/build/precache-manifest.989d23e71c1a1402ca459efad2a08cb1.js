bf690735f92ee7122c67cb589e38468aaf04355f
Raghavendiran-2002 <raghavendiran46461@gmail.com>
2024-03-28 12:00:05 +0530

    Frontend-React init

---
+++ Frontend/build/precache-manifest.989d23e71c1a1402ca459efad2a08cb1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e4ffdb075d30d938ac5c451e72f5ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bc6671509f27f83991d",
+    "url": "/static/css/main.9e8f2a23.chunk.css"
+  },
+  {
+    "revision": "258d1bdb867b331bd057",
+    "url": "/static/js/2.87ef24b1.chunk.js"
+  },
+  {
+    "revision": "fc00bc768ebd527aa418",
+    "url": "/static/js/3.20abe59b.chunk.js"
+  },
+  {
+    "revision": "7bc6671509f27f83991d",
+    "url": "/static/js/main.5f42eaab.chunk.js"
+  },
+  {
+    "revision": "28b36887796d7fb60a87",
+    "url": "/static/js/runtime~main.9bf6d4d4.js"
+  },
+  {
+    "revision": "a4b6105397e14b91956abda678c67274",
+    "url": "/static/media/Logo2.a4b61053.png"
+  },
+  {
+    "revision": "97ca5861d74791f1d74516b537116a26",
+    "url": "/static/media/wallpaper.97ca5861.jpg"
+  },
+  {
+    "revision": "8717e91e7564866ad8f0a8dc3196a5ae",
+    "url": "/static/media/wallpaper2.8717e91e.jpg"
+  }
+]);
